4815354467813b966aa64325740cca47e4256ac3
Cedric Wider <wider.cedric@gmail.com>
2023-04-23 14:51:49 +0200

    Remove unnecessary comments

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,9 +1,7 @@
 return {
-  -- customize alpha options
   {
     "goolord/alpha-nvim",
     opts = function(_, opts)
-      -- customize the dashboard header
       opts.section.header.val = {
         [[                      /^--^\     /^--^\     /^--^\]],
         [[                      \____/     \____/     \____/]],
