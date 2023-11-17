cbf0f001268ce44f6b62d459717414f73cd4db4b
Cedric Wider <wider.cedric@gmail.com>
2023-11-17 18:53:34 +0100

    update: update neotest-jest configuration
    Finally a running configuration for jest tests.

--- plugins/neotest.lua
+++ plugins/neotest.lua
@@ -14,14 +14,22 @@ return {
         adapters = {
           require "neotest-jest" {
             -- jest options
-            jestCommand = "npm run test --",
-            jestConfigFile = "jest.config.js",
+            jestCommand = "npm run test:unit --",
+            jestConfigFile = function()
+              local currentFile = vim.fn.expand "%:p"
+              return vim.fs.find({ "jest.config.js", "jest-unit.ts" }, {
+                upward = true,
+                stop = vim.fn.getcwd(),
+                path = vim.fs.dirname(currentFile),
+              })[0]
+            end,
             cwd = function(_) return vim.fn.getcwd() end,
           },
           require "neotest-vitest" {
-            -- vitestCommand = "npm test --",
+            vitestCommand = "npm test --",
             cwd = function(_) return vim.fn.getcwd() end,
           },
+
           require "neotest-rspec" {
             rspec_cmd = function()
               return vim.tbl_flatten {
