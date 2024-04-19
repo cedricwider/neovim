032c42c4990deb225f57ecceebc22daf6cc64f9e
Cedric Wider <wider.cedric@gmail.com>
2024-04-19 15:00:28 +0200

    feat: streamline mason
    It's a beast to configure, but once you've done it, it's very, very nice.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -6,6 +6,7 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = { "lua_ls", "volar", "tsserver", "solargraph" },
+      automatic_installation = { exclude = { "rust_analyzer" } },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
